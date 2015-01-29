ea825ca3c5f0d0bf19e20015b4f1d6f77cf4087a
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2015-01-29 09:06:08 +0100

    Commit CC

--- src/colaboracionmerge/ColaboracionMerge.java
+++ src/colaboracionmerge/ColaboracionMerge.java
@@ -6,6 +6,7 @@ public class ColaboracionMerge {
 
     public static void main(String[] args) {
         System.out.println("Hello Paco");
+        System.out.println("Hello madrefoka");
     }
     
 }
